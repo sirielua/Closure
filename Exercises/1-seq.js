cb13162679a141133cffa6d2e11f75f96a11abdc
sairin <siriel88@gmail.com>
2020-04-01 01:16:06 +0300

    Work Complete

--- Exercises/1-seq.js
+++ Exercises/1-seq.js
@@ -1,5 +1,29 @@
 'use strict';
 
-const seq = f => g => x => 0;
+const myseq = (...args) => arg => {
+  const type = typeof arg;
+  if (type === 'number') {
+    return args.reduce((carry, fn) => fn(carry), arg);
+  } else {
+    args.unshift(arg);
+    return myseq(...args);
+  }
+};
+
+const seq = f => y => (typeof y === 'number' ? f(y) : seq(x => f(y(x))));
+
+const fns = [seq, myseq];
+
+fns.forEach(fn => {
+  console.log(`Running ${fn.name}`);
+
+  const res1 = fn(x => x + 7)(x => x * 2)(5); // 17
+  const res2 = fn(x => x * 2)(x => x + 7)(5); // 24
+  const res3 = fn(x => x + 1)(x => x * 2)(x => x / 3)(x => x - 4)(7); // 3
+
+  console.log(res1);
+  console.log(res2);
+  console.log(res3);
+});
 
 module.exports = { seq };
--- Exercises/2-array.js
+++ Exercises/2-array.js
@@ -1,5 +1,29 @@
 'use strict';
 
-const array = () => null;
+const array = (...values) => {
+  const getter = key => values[key];
+
+  getter.push = value => values.push(value);
+  getter.pop = () => values.pop();
+  
+  return getter;
+};
+
+const arr = array('first', 'second');
+
+arr.push('third');
+arr.push('forth');
+
+console.log(arr(0)); // Выводит: first
+console.log(arr(1)); // Выводит: second
+console.log(arr(2)); // Выводит: third
+console.log(arr(3)); // Выводит: forth
+
+console.log(arr.pop()); // Выводит: forth
+console.log(arr.pop()); // Выводит: third
+console.log(arr.pop()); // Выводит: second
+console.log(arr.pop()); // Выводит: first
+
+console.log(arr.pop()); // Выводит: undefined
 
 module.exports = { array };
